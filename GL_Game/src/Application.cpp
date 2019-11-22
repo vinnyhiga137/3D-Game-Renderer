b1d9d025713015529b1e8b2169bc54eafa874ddf
Vinícius Hiroshi Higa <vinnyhiga137@hotmail.com>
2019-11-21 21:14:57 -0300

    EBO Sample added

--- GL_Game/src/Application.cpp
+++ GL_Game/src/Application.cpp
@@ -3,6 +3,51 @@
 #include <iostream>
 #include "GL/Window.h"
 #include "Events/InputEvent.h"
+#include <string>
+
+unsigned int createShaderAndCompile(unsigned int type, const char* sourceCode) {
+
+	unsigned int shader;
+	shader = glCreateShader(type);
+
+	glShaderSource(shader, 1, &sourceCode, NULL);
+	glCompileShader(shader);
+
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(shader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+		return 0;
+	}
+
+	return shader;
+}
+
+unsigned int createShaderProgram(unsigned int* vertexShader, unsigned int* fragmentShader) {
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, *vertexShader);
+	glAttachShader(shaderProgram, *fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	int  success;
+	char infoLog[512];
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINK_FAILED\n" << infoLog << std::endl;
+		return 0;
+	}
+
+	return shaderProgram;
+}
 
 int main() {
 
@@ -10,28 +55,108 @@ int main() {
 
     GLFWwindow* window = Engine::Window::instantiate();
 
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
-    };
     
-    unsigned int VBO;
-    glGenBuffers(1, &VBO); // Creating ID for the VBO (Object)
-    glBindBuffer(GL_ARRAY_BUFFER, VBO); //
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // Sending to the memory buffer the VBO's data
-    
+    unsigned int VBO; // Vertex Buffer Object (Holds m vertices which can form a triangle)
+	unsigned int VAO; // Vertex Array Object (Holds n VBOs, which can be n triangles)
+	unsigned int EBO; // Elements Buffer Object (Holds m vertices which can form a triangle OR quad...)
+
+	//float vertices[] = {
+	//	-0.5f, -0.5f, 0.0f, // Vertex 1
+	//	 0.5f, -0.5f, 0.0f, // Vertex 2
+	//	 0.0f,  0.5f, 0.0f  // Vertex 3
+	//};
+
+	float vertices[] = {
+	 0.5f,  0.5f, 0.0f,  // top right
+	 0.5f, -0.5f, 0.0f,  // bottom right
+	-0.5f, -0.5f, 0.0f,  // bottom left
+	-0.5f,  0.5f, 0.0f   // top left 
+	};
+
+	unsigned int indices[] = {  // The index always will start from ZERO
+		0, 1, 3,				// First triangle
+		1, 2, 3					// Second triangle
+	};
+
+	glGenVertexArrays(1, &VAO); // Creating "one" Vertex Array Object
+    glGenBuffers(1, &VBO);		// Creating "one" Vertex Buffer Object
+	glGenBuffers(1, &EBO);		// Creating "one" Elements Buffer Object
+
+	// WARNING! We must bind the VAO first before anything related to VBO
+	glBindVertexArray(VAO);													   // Signalizing to GPU that "VAO" must be used now
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);										   // Signalizing to GPU that "VBO" must be used now
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // Sending to the memory buffer the VBO's data
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
+	// VERTEX SHADER RELATED CODE
+	const char* vertexShaderCode =
+		"#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"\n"
+		"void main() {\n"
+		"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}";
+
+	// FRAGMENT SHADER RELATED CODE
+	const char* fragmentShaderCode =
+		"#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"\n"
+		"void main() {\n"
+		"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}";
+
+
+
+	// Compiling shaders
+	unsigned int vertexShader = createShaderAndCompile(GL_VERTEX_SHADER, vertexShaderCode);
+	unsigned int fragmentShader = createShaderAndCompile(GL_FRAGMENT_SHADER, fragmentShaderCode);
+
+	if (vertexShader == 0 || fragmentShader == 0) {
+		std::cout << "Shaders were not compiled successfully!" << std::endl;
+		return -1;
+	}
+
+
+	// Creating shader program
+	unsigned int shaderProgram = createShaderProgram(&vertexShader, &fragmentShader);
+
+	if (shaderProgram == 0) {
+		std::cout << "Shader program failed to be created!" << std::endl;
+		return -1;
+	}
+
+
+	// TELLING THE GPU HOW TO INTERPRET THE VERTEX SHADER ATTRIBUTES
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // Setting the first pointer read parameters
+	glEnableVertexAttribArray(0);												  // Saying which vertex attrib must be handled (which is ZERO)
+
 	// MAIN LOOP
 	while (!glfwWindowShouldClose(window)) {
         
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // Setting the desired color on the background
-        glClear(GL_COLOR_BUFFER_BIT); // Painting the with the clearColor parameters
+		Engine::InputEvent::processInput(window);  // Checking for interruptions
 
-        Engine::InputEvent::processInput(window);  // Checking for interruptions
-        
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // Setting the desired color on the background
+        glClear(GL_COLOR_BUFFER_BIT);		  // Painting the with the clearColor parameters
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		//glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); // Optional?
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
 		glfwSwapBuffers(window); // Signalize the GPU to render another "frame" into the screen
-		glfwPollEvents(); // Signalize tha application that an event was dispatched
+		glfwPollEvents();		 // Signalize tha application that an event was dispatched
 	}
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
 	glfwTerminate();
 
---
+++ GL_Game/src/GL/Shaders/ShaderHandler.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace Engine {
+
+	class ShaderHandler {
+	public:
+		//static unsigned int generateProgram(unsigned int vertex);
+	};
+
+}
