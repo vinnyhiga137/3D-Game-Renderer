c7e54b6f82756b8dd520660dd54716af6748d42a
Vinícius Hiroshi Higa <vinnyhiga137@hotmail.com>
2019-12-11 17:52:14 -0300

    Bug Fix - Lamp File was missing on last Commit

---
+++ GL_Game/src/Entities/Debug/Lamp.cpp
@@ -0,0 +1,100 @@
+﻿#include "Lamp.h"
+
+// Warning! Workaround here! DO NOT USE IN PRODUCTION ENV!
+Engine::Debug::Lamp::Lamp(glm::vec3 position, Texture2D* texture) {
+
+	this->position = position;
+
+	this->texture = texture;
+
+	float verticesData[] = {
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+	};
+
+
+	Engine::MemoryManagement::generateLightEmitterBuffer(verticesData, sizeof(verticesData), &(this->VAO));
+	
+}
+
+
+
+
+
+
+void Engine::Debug::Lamp::draw() {
+
+	// Updating the VAO (Vertex Array Object) 
+	glBindVertexArray(this->VAO);
+
+	//----------------------------------------------------------------//
+	// The next steps we need to the deal with the following equation //
+	//																  //
+	// Vclip=Mprojection⋅Mview⋅Mmodel⋅Vlocal							  //
+	//----------------------------------------------------------------//
+
+
+	glm::mat4 model = glm::mat4(1.0f);
+	model = glm::translate(model, glm::vec3(this->position.x, this->position.y, this->position.z));
+
+	// Sending into the shader the updated model matrix
+	this->texture->shader->setMat4Uniform("model", model);
+
+
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+
+}
+
+
+
+
+void Engine::Debug::Lamp::setPosition(glm::vec3 position) {
+    this->position = position;
+}
+
+
+
+
+
+glm::vec3 Engine::Debug::Lamp::getPosition() {
+    return this->position;
+}
---
+++ GL_Game/src/Entities/Debug/Lamp.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <glad/glad.h>
+
+#include "../../GL/MemoryManagement.h"
+#include "../../Textures/Texture2D.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+namespace Engine::Debug {
+
+	// WARNING! This a debug class! Do not use in production environment!
+	// There are workarounds on the following functions!
+	class Lamp {
+
+	private:
+
+		// Attributes
+		unsigned int VAO;
+		Texture2D* texture;
+		glm::vec3 position;
+
+	public:
+
+		Lamp(glm::vec3 position, Texture2D* texture);
+
+		void setPosition(glm::vec3 position);
+		glm::vec3 getPosition();
+		void draw();
+	};
+
+}
